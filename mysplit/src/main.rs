05b12dff15b8614fa8f2e3cb3db2afc23a76f892
Joseph Ryan <josephryan3.14@gmail.com>
2021-10-11 16:54:03 -0700

    Read extra info

--- mysplit/src/main.rs
+++ mysplit/src/main.rs
@@ -2,10 +2,7 @@
 use nix::sys::uio::{self, IoVec, RemoteIoVec};
 use nix::unistd::Pid;
 use num_bytes::FromBytes;
-use std::{
-    ffi::{CStr, CString},
-    mem, ptr, thread, time,
-};
+use std::{ffi::CString, mem, ptr, slice, thread, time};
 use sysinfo::{ProcessExt, System, SystemExt};
 
 fn read_mem(pid: Pid, base: u64, len: u64, buf: &mut [u8]) -> nix::Result<usize> {
@@ -109,7 +106,7 @@ fn class_field_offset(pid: Pid, class: u64, name: &str) -> u32 {
     let mut fields = vec![MonoClassField::default(); num_fields as usize];
     let total_size = mem::size_of::<MonoClassField>() as u64 * fields.len() as u64;
     read_mem(pid, fields_addr, total_size, unsafe {
-        std::slice::from_raw_parts_mut::<u8>(
+        slice::from_raw_parts_mut::<u8>(
             fields.as_mut_ptr() as *mut u8,
             total_size as usize,
         )
@@ -151,6 +148,27 @@ fn static_field<T: FromBytes<LEN>, const LEN: usize>(
 fn locate_splitter_info(pid: Pid, instance: u64) -> u64 {
     let splitter_instance: u64 = instance_field(pid, instance, "AutoSplitterInfo");
     splitter_instance + 0x10
+}
+
+fn read_boxed_string(pid: Pid, instance: u64) -> String {
+    let class = instance_class(pid, instance);
+    let data_offset = class_field_offset(pid, class, "m_firstChar");
+    let size_offset = class_field_offset(pid, class, "m_stringLength");
+    let size: u32 = read(pid, instance + size_offset as u64);
+    let mut oversize_buf = vec![0u8; size as usize * 2];
+    read_mem(
+        pid,
+        instance + data_offset as u64,
+        size as u64 * 2,
+        &mut oversize_buf,
+    )
+    .unwrap();
+    String::from_utf16_lossy(unsafe {
+        slice::from_raw_parts_mut::<u16>(
+            oversize_buf.as_mut_ptr() as *mut u16,
+            size as usize,
+        )
+    })
 }
 
 #[repr(C)]
@@ -201,11 +219,11 @@ fn main() -> Result<(), &'static str> {
         return Err("This is not Celeste!");
     }
     let celeste_domain = if second_domain != 0 {
-        let second_name = read_string(pid, read(pid, first_domain + 0xd8));
-        println!("Connected to: {}", second_name);
+        // let second_name = read_string(pid, read(pid, first_domain + 0xd8));
+        // println!("Connected to: {}", second_name);
         second_domain
     } else {
-        println!("Connected to: {}", first_name);
+        // println!("Connected to: {}", first_name);
         first_domain
     };
 
@@ -220,14 +238,90 @@ fn main() -> Result<(), &'static str> {
     let info_addr = locate_splitter_info(pid, celeste_instance);
     let info_size = mem::size_of::<AutoSplitterInfo>();
     let mut buf = vec![0u8; info_size];
+    let mut prev_save = 0;
+    let mut mode_stats = 0;
+    let mut death_count: u32 = 0;
+    let mut checkpoint: u32 = 0;
     loop {
         thread::sleep(time::Duration::from_millis(500));
-        read_mem(pid, info_addr, info_size as u64, &mut buf).unwrap();
+
+        if read_mem(pid, info_addr, info_size as u64, &mut buf).is_err() {
+            break;
+        }
         let asi: AutoSplitterInfo = unsafe { ptr::read(buf.as_ptr() as *const _) };
+
+        let current_level = if asi.level != 0 {
+            read_boxed_string(pid, asi.level)
+        } else {
+            String::new()
+        };
+
+        let save_addr = static_field(pid, save_data, "Instance");
+        if save_addr != 0 {
+            if save_addr != prev_save {
+                thread::sleep(time::Duration::from_secs(2));
+                prev_save = save_addr;
+                mode_stats = 0;
+                continue;
+            }
+            death_count = instance_field(pid, save_addr, "TotalDeaths");
+            if asi.chapter == -1 {
+                mode_stats = 0;
+            } else if mode_stats == 0 {
+                let areas_obj: u64 = instance_field(pid, save_addr, "Areas");
+                let size: u32 = instance_field(pid, areas_obj, "_size");
+                let areas_arr: u64 = if size == 11 {
+                    // println!("Passed");
+                    instance_field(pid, areas_obj, "_items")
+                } else {
+                    // println!("Failed");
+                    0
+                };
+                if areas_arr != 0 {
+                    // println!("Areas arr: {:x}", areas_arr);
+                    let area_stats: u64 =
+                        read(pid, areas_arr + 0x20 + asi.chapter as u64 * 8);
+                    // println!("Area stats: {:x}", area_stats);
+                    let mode_arr =
+                        instance_field::<u64, 8>(pid, area_stats, "Modes") + 0x20;
+                    mode_stats = read(pid, mode_arr + asi.mode as u64 * 8);
+                }
+            }
+            // println!("Mode stats: {:x}", mode_stats);
+            if mode_stats != 0 {
+                let checkpoints_obj = instance_field(pid, mode_stats, "Checkpoints");
+                // println!("checkpoint obj: {:x}", checkpoints_obj);
+                checkpoint = instance_field(pid, checkpoints_obj, "_count");
+            }
+        }
+
+        let in_cutscene = if asi.chapter != -1 {
+            if !asi.chapter_started || asi.chapter_complete {
+                true
+            } else {
+                let scene = read(
+                    pid,
+                    celeste_instance + class_field_offset(pid, engine, "scene") as u64,
+                );
+                if instance_class(pid, scene) != level {
+                    false
+                } else {
+                    let byte: u8 = read(
+                        pid,
+                        scene + class_field_offset(pid, level, "InCutscene") as u64,
+                    );
+                    byte != 0
+                }
+            }
+        } else {
+            false
+        };
+
+        dbg!(in_cutscene);
+        dbg!(checkpoint);
+        dbg!(death_count);
+        dbg!(current_level);
         dbg!(asi.file_time);
     }
-    // println!("{:x}", read::<u64>(pid, base_addr));
-    // let mut buf = Vec::with_capacity(128);
-    // println!("{:?}", read_mem(pid, 54, 32, &mut buf))
     Ok(())
 }
