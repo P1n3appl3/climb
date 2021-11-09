1331f7d43bca1fbb07de2f3be704d9b3d8fdf478
Joseph Ryan <josephryan3.14@gmail.com>
2021-11-09 03:02:58 -0800

    Tweak interface

--- auto-splitter/src/lib.rs
+++ auto-splitter/src/lib.rs
@@ -1,34 +1,51 @@
 #[macro_use]
 mod livesplit;
 
-use livesplit::TimerState;
+use livesplit::{Process, Splitter, TimerState};
 
-fn init() {
-    livesplit::print("start");
+#[derive(Debug, Default)]
+struct MySplitter {
+    process: Option<Process>,
 }
 
-fn setup() {
-    livesplit::attach("Celeste.bin.x86");
-    livesplit::set_tick_rate(60.0);
-    livesplit::set_variable("foo", "bar");
-    livesplit::print("configured autosplitter");
-}
+register_hooks!(MySplitter);
+impl Splitter for MySplitter {
+    fn new() -> Self {
+        let mut splitter = MySplitter::default();
+        splitter.process = splitter.attach("Celeste.bin.x86");
+        splitter.print("splitter initialized");
+        splitter.set_tick_rate(120.0);
+        splitter.set_variable("deaths", "9999");
+        splitter.print("configured autosplitter");
+        splitter
+    }
 
-fn tick() {
-    livesplit::print("update");
-    // livesplit::start();
-    // livesplit::split();
-    // livesplit::reset();
-    // livesplit::pause();
-    // livesplit::unpause();
-    // livesplit::detach();
-    livesplit::set_game_time(1.234);
-    match livesplit::get_state() {
-        TimerState::NotRunning => {}
-        TimerState::Running => {}
-        TimerState::Paused => {}
-        TimerState::Finished => {}
+    fn update(&mut self) {
+        self.print("update");
+
+        self.start();
+        self.split();
+        self.reset();
+        self.pause();
+        self.unpause();
+
+        self.set_game_time(1.234);
+
+        let mut buf = vec![0u8; 22];
+        if let Some(p) = &self.process {
+            if let Some(addr) = p.module("some_mod") {
+                if p.read_into_buf(0xdeadbeef, &mut buf[..5]).is_err() {
+                    self.print("Failed to read addr")
+                }
+                dbg!(p.read::<i128>(addr).ok());
+            }
+        }
+
+        match self.state() {
+            TimerState::NotRunning => {}
+            TimerState::Running => {}
+            TimerState::Paused => {}
+            TimerState::Finished => {}
+        }
     }
 }
-
-register_hooks!(init, setup, tick);
